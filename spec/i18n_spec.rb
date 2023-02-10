4f3382d6fb2d8bd29b471bf056644cfcd1606fcf
Ivan Vergés <ivan@pokecode.net>
2023-02-10 11:33:33 +0100

    rubocop

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-require "i18n/tasks"
+require 'i18n/tasks'
 
-describe "I18n sanity" do
+describe 'I18n sanity' do
   let(:locales) do
-    ENV["ENFORCED_LOCALES"].presence || "en"
+    ENV['ENFORCED_LOCALES'].presence || 'en'
   end
 
-  let(:i18n) { I18n::Tasks::BaseTask.new({ locales: locales.split(",") }) }
+  let(:i18n) { I18n::Tasks::BaseTask.new({ locales: locales.split(',') }) }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
   let(:non_normalized_paths) { i18n.non_normalized_paths }
 
-  it "does not have missing keys" do
+  it 'does not have missing keys' do
     expect(missing_keys).to be_empty, "#{missing_keys.inspect} are missing"
   end
 
@@ -20,8 +20,8 @@ describe "I18n sanity" do
   #   expect(unused_keys).to be_empty, "#{unused_keys.inspect} are unused"
   # end
 
-  unless ENV["SKIP_NORMALIZATION"]
-    it "is normalized" do
+  unless ENV['SKIP_NORMALIZATION']
+    it 'is normalized' do
       error_message = "The following files need to be normalized:\n" \
                       "#{non_normalized_paths.map { |path| "  #{path}" }.join("\n")}\n" \
                       "Please run `bundle exec i18n-tasks normalize --locales #{locales}` to fix them"
