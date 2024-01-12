b7b5dbf2a0c3dc536f9c0341de22f2e159c61c15
Elvia Benedith <ejbenedith@gmail.com>
2024-01-12 13:20:09 +0100

    fix spec tets

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -9,7 +9,7 @@ checksums = [
   {
     package: "decidim-admin",
     files: {
-      "/app/controllers/decidim/admin/newsletters_controller.rb" => "cadf58205b79ea4b45b032014705af13",
+      "/app/controllers/decidim/admin/newsletters_controller.rb" => "0dae3a6330f871f37a91b7787b698a99",
       "/app/controllers/decidim/admin/newsletter_templates_controller.rb" => "fc3eb1131f62719a822823cd2cab3f5d"
     }
   }
