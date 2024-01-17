1551ba1e042a6b423dd6f988286e3cb8208e7272
Elvia Benedith <ejbenedith@gmail.com>
2024-01-17 10:08:56 +0100

    add styles to accordion

--- app/packs/entrypoints/decidim_newsletter_agenda.js
+++ app/packs/entrypoints/decidim_newsletter_agenda.js
@@ -3,3 +3,23 @@ import "./decidim_newsletter_agenda.scss";
 // Images
 require.context("../images", true)
 require.context("../fonts", true)
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+    const accordionItems = document.querySelectorAll('.accordion-item');
+    console.log("hola");
+    accordionItems.forEach(function (item) {
+
+      const title = item.querySelector('.accordion-title');
+      let content = item.querySelector('.accordion-content');
+      console.log(content.style.display);
+      content.style.display = 'none';
+
+      title.addEventListener('click', function () {
+
+        content = item.querySelector('.accordion-content');
+        content.style.display = content.style.display === 'none' ? 'block' : 'none';
+      });
+    });
+});
