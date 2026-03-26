14a66153b1d12fbeb3982edb5c82337ee16a0f93
David Igón <david@pokecode.net>
2026-03-26 12:35:34 +0100

    bump module version to 3.0

--- lib/decidim/newsletter_agenda/version.rb
+++ lib/decidim/newsletter_agenda/version.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module NewsletterAgenda
-    VERSION = "2.1"
+    VERSION = "3.0"
     DECIDIM_VERSION = { github: "decidim/decidim", branch: "release/0.30-stable" }.freeze
     COMPAT_DECIDIM_VERSION = [">= 0.30", "< 0.32"].freeze
   end
