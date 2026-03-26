b7ebf053b072410cc2018cbce57394aaf4c0179d
David Igón <david@pokecode.net>
2026-03-26 11:40:31 +0100

    update references to decidim 0.30

--- lib/decidim/newsletter_agenda/version.rb
+++ lib/decidim/newsletter_agenda/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   module NewsletterAgenda
     VERSION = "2.1"
-    DECIDIM_VERSION = { github: "decidim/decidim", branch: "release/0.29-stable" }.freeze
-    COMPAT_DECIDIM_VERSION = [">= 0.29", "< 0.30"].freeze
+    DECIDIM_VERSION = { github: "decidim/decidim", branch: "release/0.30-stable" }.freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.30", "< 0.32"].freeze
   end
 end
